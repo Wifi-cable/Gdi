59bde9e98c5fdec6a37c6fb673916bec664cc7ff
mary <Wifi-cable@users.noreply.github.com>
2016-04-02 10:34:12 +0200

    first popup

---
+++ Kap2Eclipse/src/MeineGUI.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class MeineGUI extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MeineGUI f= new MeineGUI();
+		f.setSize(300, 200);
+		f.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		f.setVisible(true);
+	}
+
+}
