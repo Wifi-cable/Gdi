70d47758f6c525517de9c2c6a3ac84ff84c4e6f2
mary <Wifi-cable@users.noreply.github.com>
2016-04-15 14:57:38 +0200

    recursion sillynes

---
+++ crashKurs/src/crashKurs/Recoursion.java
@@ -0,0 +1,13 @@
+package crashKurs;
+
+public class Recoursion {
+
+	public static void main(String[] args) {
+		
+		for (int i=0; i < 10; i++){ // counts from 0 to 9, because i is the variable used
+			System.out.println("langweilig"+i); //displays "langweilig" and the number
+		} // it does that because it starts at zero and adds 1 untill it reaches nine.
+			
+	}
+}
+
