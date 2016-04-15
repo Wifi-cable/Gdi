044fc062dee80180158f0ce8f68f23d6d6864680
mary <Wifi-cable@users.noreply.github.com>
2016-04-15 14:19:45 +0200

    sudoku bugs orig professor

---
+++ crashKurs/src/crashKurs/uebung.java
@@ -0,0 +1,24 @@
+package crashKurs;
+
+import java.util.Scanner;
+
+public class uebung {
+
+	public static void main(String[] args) {
+		char digit (j= {'0','1','2','3','4','5','6','7','8'});
+		scanner sc= new scanner (System.in);
+		System.out.println("bitte zahl eingeben");
+		string uebungSudoku= sc next line();
+		
+		int sudoku[9] [9] new int [9][9]
+				for (int y=0;) y<9; y++){
+					for ( int x=0; x<9; x++){
+				setze [y][x]=0;}
+				}
+	}
+		
+		
+
+	}
+
+}
